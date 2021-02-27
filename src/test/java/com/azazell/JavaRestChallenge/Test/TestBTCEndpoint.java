1308d7fd734364a17750b7369449c546aff81fb4
Azazell <azazellfn@gmail.com>
2021-02-27 22:12:21 +0300

    improve TestBTCEndpoint case

--- src/test/java/com/azazell/JavaRestChallenge/Test/TestBTCEndpoint.java
+++ src/test/java/com/azazell/JavaRestChallenge/Test/TestBTCEndpoint.java
@@ -13,13 +13,22 @@ import java.net.URISyntaxException;
  */
 public class TestBTCEndpoint {
     @Test
-    public void testBTCEndpointBuilder() throws URISyntaxException {
+    public void testBTCEndpointBuilder() {
         // Check if builder throws exception if we build object improperly
         Assertions.assertThrows(IllegalStateException.class, () -> {
             final BTCTickerEndpoint endpoint = BTCTickerEndpoint.newBuilder()
                     .uri(new URI("https://example.com"))
                     .parser(new BlockchainParser())
                     //.name("test") provide no name to test if it throws exception
+                    .build();
+        });
+
+        // Check if builder doesn't throw exception if we build object properly
+        Assertions.assertDoesNotThrow(() -> {
+            final BTCTickerEndpoint endpoint = BTCTickerEndpoint.newBuilder()
+                    .uri(new URI("https://example.com"))
+                    .parser(new BlockchainParser())
+                    .name("test")
                     .build();
         });
     }
